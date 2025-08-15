c623732889e60d135877f3e313076abb5ae0dd2d
monisha33345 <monishareddy0912@gmail.com>
2025-08-15 17:25:55 +0530

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+bind = "0.0.0.0:" + str(os.environ.get("PORT", 8000))
+workers = 2
+worker_class = "sync"
+worker_connections = 1000
+timeout = 120
+keepalive = 2
+max_requests = 1000
+max_requests_jitter = 100
+preload_app = True
